e0f23c678a5e75f68a5940710f156f747c236626
sugar <821293028@qq.com>
2021-03-01 15:06:51 +0000

    MACDAVG and its Test

--- app/src/main/java/uk/ac/kcl/sufcwmillionapplication/indicators/MACDAVGIndicators.java
+++ app/src/main/java/uk/ac/kcl/sufcwmillionapplication/indicators/MACDAVGIndicators.java
@@ -12,37 +12,30 @@ public class MACDAVGIndicators extends TechnicalIndicators {
     public List<CalculateResult> calculate(List<DailyQuote> dailyQuoteList) {
         //the result set
         List<CalculateResult> result = new ArrayList<>();
-        //The first MACDAVG_EMA equals the close price of the first day
-        CalculateResult cal = new CalculateResult();
-        String Date = super.extractLastDate(dailyQuoteList,dailyQuoteList.size()).get(0);
-        cal.date = Date;
-        Double LastEma = super.extractLastClosingPrice(dailyQuoteList,dailyQuoteList.size()).get(0);
-        cal.data = LastEma;
-        result.add(cal);
 
         //calculate MACD
         EMAIndicators ema26 = new EMAIndicators(26);
+        List<CalculateResult> res26 = ema26.calculate(dailyQuoteList);
         EMAIndicators ema12 = new EMAIndicators(12);
         List<CalculateResult> res12 = ema12.calculate(dailyQuoteList);
-        List<CalculateResult> res26 = ema26.calculate(dailyQuoteList);
-        List<CalculateResult> MACD_results = new ArrayList<>();
-        CalculateResult cal_MACDAVG = new CalculateResult();
+        //The first MACDAVG_EMA, equals the MACD of the first day
+        double LastEma=res12.get(0).data - res26.get(0).data;
         for(int i = 0; i < res12.size(); i++){
+            CalculateResult cal_MACDAVG = new CalculateResult();
             double value1 = res12.get(i).data;
             double value2 = res26.get(i).data;
-            // insert date
             cal_MACDAVG.date = res12.get(i).date;
-            // caluculate MACD
-            double MACD_result = value1 - value2;
+            double MACD_result= value1 - value2;
+            System.out.println("macd:"+MACD_result);
             // caluculate MACDAVG_EMA, term=9
             Double[] EMAIndex = GetEMAIndex(9);
+            System.out.println("lastema:"+LastEma);
             Double MACDAVG_EMA = EMAIndex[0] * MACD_result + EMAIndex[1] * LastEma;
             // insert calculated data
             cal_MACDAVG.data = MACDAVG_EMA;
             result.add(cal_MACDAVG);
             // change the lastema
             LastEma = MACDAVG_EMA;
-
         }
         return result;
     }
