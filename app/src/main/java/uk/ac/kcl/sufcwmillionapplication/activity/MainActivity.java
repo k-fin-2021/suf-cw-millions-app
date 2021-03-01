e8b8d3c0ef60199c15da2f3ba7f3a999a4a35d39
sidan26 <825662106@qq.com>
2021-03-01 09:50:02 +0000

    [Feature] UI and unit test update
    * refix the UI
    
    * add new UI and SEA
    
    * update the date for get data
    
    * repair
    
    * feature ui
    
    * fix
    
    * the last ui
    
    * clear
    
    * sma unit test

--- app/src/main/java/uk/ac/kcl/sufcwmillionapplication/activity/MainActivity.java
+++ app/src/main/java/uk/ac/kcl/sufcwmillionapplication/activity/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
--- app/src/main/java/uk/ac/kcl/sufcwmillionapplication/utils/SPUtils.java
+++ app/src/main/java/uk/ac/kcl/sufcwmillionapplication/utils/SPUtils.java
@@ -27,7 +27,7 @@ public final class SPUtils {
     private final static int mode = Context.MODE_PRIVATE;
 
     /**
-     * 保存首选项
+     *
      *
      * @param context
      * @param key
@@ -56,7 +56,7 @@ public final class SPUtils {
 
 
     /**
-     * 获取首选项
+     *
      *
      * @param context
      * @param key
@@ -79,7 +79,7 @@ public final class SPUtils {
     }
 
     /**
-     * desc:将数组转为16进制
+     *
      *
      * @param bArray
      * @return modified:
@@ -103,34 +103,28 @@ public final class SPUtils {
     }
 
     /**
-     * desc:保存对象
+     *
      *
      * @param context
      * @param key
-     * @param obj     要保存的对象，只能保存实现了serializable的对象 modified:
+     * @param obj
      */
     public static void saveObject(Context context, String key, Object obj) {
         try {
-            // 保存对象
             SharedPreferences.Editor sharedata = context.getSharedPreferences("user", Context.MODE_PRIVATE).edit();
-            // 先将序列化结果写到byte缓存中，其实就分配一个内存空间
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
             ObjectOutputStream os = new ObjectOutputStream(bos);
-            // 将对象序列化写入byte缓存
             os.writeObject(obj);
-            // 将序列化的数据转为16进制保存
             String bytesToHexString = bytesToHexString(bos.toByteArray());
-            // 保存该16进制数组
             sharedata.putString(key, bytesToHexString);
             sharedata.commit();
         } catch (IOException e) {
             e.printStackTrace();
-            //Log.e("", "保存obj失败");
         }
     }
 
     /**
-     * desc:将16进制的数据转为数组
+     * desc:
      *
      * @param data
      * @return modified:
@@ -142,32 +136,32 @@ public final class SPUtils {
         }
         byte[] retData = new byte[hexString.length() / 2];
         for (int i = 0; i < hexString.length(); i++) {
-            int int_ch; // 两位16进制数转化后的10进制数
-            char hex_char1 = hexString.charAt(i); // 两位16进制数中的第一位(高位*16)
+            int int_ch;
+            char hex_char1 = hexString.charAt(i);
             int int_ch1;
             if (hex_char1 >= '0' && hex_char1 <= '9')
-                int_ch1 = (hex_char1 - 48) * 16; // // 0 的Ascll - 48
+                int_ch1 = (hex_char1 - 48) * 16;
             else if (hex_char1 >= 'A' && hex_char1 <= 'F')
-                int_ch1 = (hex_char1 - 55) * 16; // // A 的Ascll - 65
+                int_ch1 = (hex_char1 - 55) * 16;
             else
                 return null;
             i++;
-            char hex_char2 = hexString.charAt(i); // /两位16进制数中的第二位(低位)
+            char hex_char2 = hexString.charAt(i);
             int int_ch2;
             if (hex_char2 >= '0' && hex_char2 <= '9')
-                int_ch2 = (hex_char2 - 48); // // 0 的Ascll - 48
+                int_ch2 = (hex_char2 - 48);
             else if (hex_char2 >= 'A' && hex_char2 <= 'F')
-                int_ch2 = hex_char2 - 55; // // A 的Ascll - 65
+                int_ch2 = hex_char2 - 55;
             else
                 return null;
             int_ch = int_ch1 + int_ch2;
-            retData[i / 2] = (byte) int_ch;// 将转化后的数放入Byte里
+            retData[i / 2] = (byte) int_ch;
         }
         return retData;
     }
 
     /**
-     * desc:获取保存的Object对象
+     *
      *
      * @param context
      * @param key
@@ -181,11 +175,9 @@ public final class SPUtils {
                 if (TextUtils.isEmpty(string)) {
                     return null;
                 } else {
-                    // 将16进制的数据转为数组，准备反序列化
                     byte[] stringToBytes = StringToBytes(string);
                     ByteArrayInputStream bis = new ByteArrayInputStream(stringToBytes);
                     ObjectInputStream is = new ObjectInputStream(bis);
-                    // 返回反序列化得到的对象
                     Object readObject = is.readObject();
                     return readObject;
                 }
@@ -197,7 +189,6 @@ public final class SPUtils {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        // 所有异常返回null
         return null;
     }
 
