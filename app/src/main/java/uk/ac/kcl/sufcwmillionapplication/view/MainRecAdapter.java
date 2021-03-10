b131ff5a03d1edc100c84711e174c55faa7b4fd8
sidan26 <825662106@qq.com>
2021-03-10 17:44:41 +0000

    add try exception for cache

--- app/src/main/java/uk/ac/kcl/sufcwmillionapplication/view/MainRecAdapter.java
+++ app/src/main/java/uk/ac/kcl/sufcwmillionapplication/view/MainRecAdapter.java
@@ -278,18 +278,28 @@ public class MainRecAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
             SymbolInfo symbolInfo;
             List<DailyQuote> quotesDisplay = new ArrayList<>();
 
-            if (CacheUtils.isCache(mContext,tmpHistory)){
-                quotesCal = CacheUtils.getCacheDailyQuote(mContext,tmpHistory);
-//                symbolInfo = CacheUtils.getCacheSymbolInfo(mContext,tmpHistory);
-                symbolInfo = shareDao.getInfoOfSymbol(tmpHistory);
-                Log.d("cache","get data from cache");
-            }else {
+            try{
+                if (CacheUtils.isCache(mContext,tmpHistory)){
+                    quotesCal = CacheUtils.getCacheDailyQuote(mContext,tmpHistory);
+                    if (quotesCal == null || quotesCal.size()==0){
+                        symbolInfo = shareDao.getInfoOfSymbol(tmpHistory);
+                        CacheUtils.updateCache(mContext,tmpHistory,quotesCal,symbolInfo);
+                    }
+                    symbolInfo = shareDao.getInfoOfSymbol(tmpHistory);
+                    Log.d("cache","get data from cache");
+                }else {
+                    quotesCal = shareDao.getHistoryQuotes(tmpHistory);
+                    symbolInfo = shareDao.getInfoOfSymbol(tmpHistory);
+                    CacheUtils.updateCache(mContext,tmpHistory,quotesCal,symbolInfo);
+                    Log.d("cache","get data from network");
+                }
+            }catch (Exception e){
                 quotesCal = shareDao.getHistoryQuotes(tmpHistory);
                 symbolInfo = shareDao.getInfoOfSymbol(tmpHistory);
                 CacheUtils.updateCache(mContext,tmpHistory,quotesCal,symbolInfo);
                 Log.d("cache","get data from network");
-            }
-//            int index = 0;
+            };
+//            int index = 0
 //            int size = quotesCal.size();
 //            while (index < size){
 //                try {
