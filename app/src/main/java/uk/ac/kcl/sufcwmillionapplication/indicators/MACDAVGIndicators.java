9d4dfaad0ea24c595e11c6af7ba001052fccaa79
TeresaTang98 <77724256+TeresaTang98@users.noreply.github.com>
2021-03-12 23:36:52 +0000

    [Improve] Modified MACDAVG and its unit test
    Co-authored-by: sugar <821293028@qq.com>

--- app/src/main/java/uk/ac/kcl/sufcwmillionapplication/indicators/MACDAVGIndicators.java
+++ app/src/main/java/uk/ac/kcl/sufcwmillionapplication/indicators/MACDAVGIndicators.java
@@ -18,26 +18,84 @@ public class MACDAVGIndicators extends TechnicalIndicators {
         List<CalculateResult> res26 = ema26.calculate(dailyQuoteList);
         EMAIndicators ema12 = (EMAIndicators) IndicatorFactory.get(IndicatorNames.EMA_12);
         List<CalculateResult> res12 = ema12.calculate(dailyQuoteList);
-        //The first MACDAVG_EMA, equals the MACD of the first day
-        double lastEma=res12.get(0).data - res26.get(0).data;
-        for(int i = 0; i < res12.size(); i++){
-            CalculateResult calMACDAVG = new CalculateResult();
-            double value1 = res12.get(i).data;
-            double value2 = res26.get(i).data;
-            calMACDAVG.date = res12.get(i).date;
-            double MACDResult= value1 - value2;
-            System.out.println("macd:"+MACDResult);
-            // caluculate MACDAVG_EMA, term=9
-            Double[] EMAIndex = getEMAIndex(9);
-            System.out.println("lastema:"+lastEma);
-            Double MACDAVG_EMA = EMAIndex[0] * MACDResult + EMAIndex[1] * lastEma;
-            // insert calculated data
-            calMACDAVG.data = MACDAVG_EMA;
-            result.add(calMACDAVG);
-            // change the lastema
-            lastEma = MACDAVG_EMA;
+        //MACDAVG_EMA of yesterday
+        double LastEma=0;
+        int counter=0;
+        int j = 0;
+        if(res12.size() > res26.size()) {
+            for (int i = 0; i < res12.size(); i++) {
+                if(!res12.get(i).date.equals(res26.get(j).date)){
+                    continue;
+                }else{
+                    CalculateResult cal_MACDAVG = new CalculateResult();
+                    double value1 = res12.get(i).data; //i
+                    double value2 = res26.get(j).data; //j
+                    j++;
+                    cal_MACDAVG.date = res12.get(i).date;
+                    double MACD_result = value1 - value2;
+                    if(counter==0) { //first date
+                        counter++;
+                        //The first MACDAVG_EMA, equals the MACD of the first day
+                        LastEma = MACD_result;
+                        System.out.println("macd:" + MACD_result);
+                        // caluculate MACDAVG_EMA, term=9
+                        Double MACDAVG_EMA = LastEma;
+                        System.out.println("MACDAVG_first:" + MACDAVG_EMA);
+                        // insert calculated data
+                        cal_MACDAVG.data = MACDAVG_EMA;
+                        result.add(cal_MACDAVG);
+                        // change the lastema
+                        LastEma = MACDAVG_EMA;
+                    }else{
+                        System.out.println("macd:" + MACD_result);
+                        // caluculate MACDAVG_EMA, term=9
+                        Double[] EMAIndex = getEMAIndex(9);
+                        Double MACDAVG_EMA = EMAIndex[0] * MACD_result + EMAIndex[1] * LastEma;
+                        System.out.println("MACDAVG:" + MACDAVG_EMA);
+                        // insert calculated data
+                        cal_MACDAVG.data = MACDAVG_EMA;
+                        result.add(cal_MACDAVG);
+                        // change the lastema
+                        LastEma = MACDAVG_EMA;
+                    }
+                }
+            }
+            return result;
+        }  //res12.size() < res26.size()
+        else {
+            for (int i = 0; i < res12.size(); i++) {
+                CalculateResult cal_MACDAVG = new CalculateResult();
+                double value1 = res12.get(i).data;
+                double value2 = res26.get(i).data;
+                cal_MACDAVG.date = res12.get(i).date;
+                double MACD_result = value1 - value2;
+                if(counter==0) { //first date
+                    counter++;
+                    //The first MACDAVG_EMA, equals the MACD of the first day
+                    LastEma = MACD_result;
+                    System.out.println("macd:" + MACD_result);
+                    // caluculate MACDAVG_EMA, term=9
+                    Double MACDAVG_EMA = LastEma;
+                    System.out.println("MACDAVG_first:" + MACDAVG_EMA);
+                    // insert calculated data
+                    cal_MACDAVG.data = MACDAVG_EMA;
+                    result.add(cal_MACDAVG);
+                    // change the lastema
+                    LastEma = MACDAVG_EMA;
+                }else{
+                    System.out.println("macd:" + MACD_result);
+                    // caluculate MACDAVG_EMA, term=9
+                    Double[] EMAIndex = getEMAIndex(9);
+                    Double MACDAVG_EMA = EMAIndex[0] * MACD_result + EMAIndex[1] * LastEma;
+                    System.out.println("MACDAVG:" + MACDAVG_EMA);
+                    // insert calculated data
+                    cal_MACDAVG.data = MACDAVG_EMA;
+                    result.add(cal_MACDAVG);
+                    // change the lastema
+                    LastEma = MACDAVG_EMA;
+                }
+            }
+            return result;
         }
-        return result;
     }
-
 }
