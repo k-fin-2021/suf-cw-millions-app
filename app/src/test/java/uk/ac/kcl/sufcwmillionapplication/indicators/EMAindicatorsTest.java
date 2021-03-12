8858e13b75e64092c0d4da38a42b47d20f28cda8
Ap-Zh <77758668+Ap-Zh@users.noreply.github.com>
2021-03-12 11:17:03 +0000

    [Feature] MACD unit test case
    * EMA Change
    
    * EMA Change
    
    * EMA fix
    
    * EMA fix
    
    * [Feature] MACD update
    
    * update MACD
    
    * add MACDIndicators test
    
    * EMATestCase
    
    Co-authored-by: Xiaoran46 <57056248+Xiaoran46@users.noreply.github.com>

--- app/src/test/java/uk/ac/kcl/sufcwmillionapplication/indicators/EMAindicatorsTest.java
+++ app/src/test/java/uk/ac/kcl/sufcwmillionapplication/indicators/EMAindicatorsTest.java
@@ -3,6 +3,8 @@ package uk.ac.kcl.sufcwmillionapplication.indicators;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +16,7 @@ public class EMAindicatorsTest {
     private EMAIndicators emaIndicators;
 
     @Test
-    public void testEMA_calculate() {
+    public void testEMA_calculate1() {
         List<DailyQuote> dailyquote = new ArrayList<DailyQuote>();
         DailyQuote dq1 = DailyQuote.createByPKDailyQuote("2020-01-01");
         dq1.open = 2.0;
@@ -59,7 +61,27 @@ public class EMAindicatorsTest {
             Assert.assertEquals(result.get(i).data,TestResult[i], 0.001);
         }
     }
-
+    @Test
+    public void testEMA_calculate2() {
+        List<DailyQuote> dailyquote = GetTestDataUtil.getData();
+        emaIndicators = new EMAIndicators(26);
+        List<CalculateResult> result = emaIndicators.calculate(dailyquote);
+        for(int i = 0; i < result.size(); i++){
+            System.out.println(result.get(i).date+", "+result.get(i).data);
+        }
+        double[] expect_result = new double[]{1.383390273713844,
+                1.3860466491808818,
+                1.387965196650876,
+                1.388095204404908,
+                1.3883304462171138,
+                1.3897071983613418,
+                1.3889536029751894,
+                1.389738996767223
+        };
+        for(int i = 0; i < result.size(); i++){
+            Assert.assertEquals(result.get(i).data,expect_result[i], 0.001);
+        }
+    }
 
     private static class TestIndicators extends TechnicalIndicators{
 
